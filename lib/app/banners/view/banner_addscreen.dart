3501a04131bc41e0f528afaa88876b28a5b88bdb
pranavpv773 <pranavpv773@gmail.com>
2022-10-03 22:37:51 +0530

    banner screen added

---
+++ lib/app/banners/view/banner_addscreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AddBannerScreen extends StatelessWidget {
+  const AddBannerScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
